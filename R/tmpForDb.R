f4048aa5d67ebe151e1c96ee77ee1d014f2dfacd
theasjblog <tequilacanaria@hotmail.it>
2021-02-04 19:51:25 +0000

    change data source

--- R/tmpForDb.R
+++ R/tmpForDb.R
@@ -1,111 +1,141 @@
-source('./R/getData.R')
 library('dplyr')
-countryOnly <- function(df){
-  myList <- split(df, df$Country)
-  myList <- lapply(myList, function(d){
-    if(nrow(d)==1){
-      res <- d
-    } else{
-      idx <- which(is.na(d$State))
-      if (length(idx)==1){
-        res <- d[idx,]
-      } else {
-        popIs <- sum(d$Population, na.rm = TRUE)
-        res <- data.frame(City = '',
-                          State = '',
-                          Country = unique(d$Ccountry),
-                          Population = popIs)
-      }
-    }
-    return(res)
-  })
-  myList <- bind_rows(myList)
-  myList <- myList %>% 
-    select(-one_of('City', 'State'))
-  myList$Country[myList$Country %in% c('US', 'United States')] <- 'United Stets of America'
-  return(myList)  
-}
+library('data.table')
+library('DBI')
+library('readxl')
 
-popData <- getPopData()
-popData <- countryOnly(popData)
-
-assembleType2 <- function(type){
-  if(type == 'cases'){
-    pathGlobal <- 'time_series_covid19_confirmed_global.csv'
-    pathUS <- 'time_series_covid19_confirmed_US.csv'
-  } else if (type == 'deaths'){
-    pathGlobal <- 'time_series_covid19_deaths_global.csv'
-    pathUS <- 'time_series_covid19_deaths_US.csv'
-  } else if (type == 'recovered'){
-    pathGlobal <- 'time_series_covid19_recovered_global.csv'
-  }
-  
-  globalData <- getGlobal(pathGlobal)
-  if(type != 'recovered'){
-    USData <- getUS(pathUS)
-    allData <- suppressWarnings(bind_rows(USData, globalData))
-  } else {
-    allData <- globalData
-    allData <- cbind(data.frame(City = rep(NA, nrow(allData))),
-                     allData)
-    allData$City <- as.character(NA)
-  }
-  
-  myList <- split(allData, allData$Country)
-  myList <- lapply(myList, function(d){
-    if(nrow(d)==1){
-      res <- d
-    } else{
-      idx <- which(is.na(d$State))
-      if (length(idx)==1){
-        res <- d[idx,]
-      } else {
-        res <- colSums(d[,seq(4,ncol(d))], na.rm = TRUE)
-        res$City <- ''
-        res$State <- ''
-        res$Country <- unique(d$Ccountry)
-      }
-    }
-    return(res)
-  })
-  myList <- bind_rows(myList)
-  myList <- myList %>% 
-    select(-one_of('City', 'State'))
-  myList$Country[myList$Country %in% c('US', 'United States')] <- 'United Stets of America'
-  colnames(myList)[seq(2,ncol(myList))] <- covid19Visualizer::getDates(colnames(myList)[seq(2,ncol(myList))])
-  
-  return(myList)
-}
-
-cases <- assembleType2('cases')
-deaths <- assembleType2('deaths')
-recovered <- assembleType2('recovered')
-library('readr')
-library(tidyr)
-getVacc <- function(){
-  vaccinations <- read_csv("covid-19-data-master/public/data/vaccinations/vaccinations.csv")
-  vaccinations <- vaccinations[order(vaccinations$date), ]
-  vaccinations$date <- as.character(vaccinations$date)
-  vaccinations <- vaccinations %>%
-    select(location, date, total_vaccinations,
-    people_vaccinated, people_fully_vaccinated,
-    daily_vaccinations)
-  total_vaccinations <- vaccinations %>% 
-    select(location, date, total_vaccinations) %>%
-    pivot_wider(names_from = date, values_from = total_vaccinations)
-  people_vaccinated <- vaccinations %>% 
-    select(location, date, people_vaccinated) %>%
-    pivot_wider(names_from = date, values_from = people_vaccinated)
-  people_fully_vaccinated <- vaccinations %>% 
-    select(location, date, people_fully_vaccinated) %>%
-    pivot_wider(names_from = date, values_from = people_fully_vaccinated)
-  allData <- bind_rows(total_vaccinations, people_vaccinated)
-  allData <- bind_rows(allData, people_fully_vaccinated)
+getAllData <- function(){
+  #load pre-curated data
+  #download.file(url='https://covid.ourworldindata.org/data/owid-covid-data.xlsx',
+  #              destfile='owid-covid-data.xlsx')
+  allData <- read_excel("owid-covid-data.xlsx",
+                        col_types = c("text", "text", "text",
+                                      "text", "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "text", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric",
+                                      "numeric", "numeric", "numeric"))
   colnames(allData)[colnames(allData)=='location'] <- 'Country'
-  allData$Country[allData$Country %in% c('US', 'United States')] <- 'United Stets of America'
+  allData$date <- as.character(allData$date)
   return(allData)
 }
-vaccinations <- getVacc()
-#to do
-# melt all table so that dates is a column
-# make sure there is a type column
+
+getPopulation <- function(allData){
+  populationData <- allData %>%
+    select(Country,
+           population,
+           population_density,
+           median_age,
+           aged_65_older,
+           aged_70_older,
+           gdp_per_capita,
+           extreme_poverty,
+           cardiovasc_death_rate,
+           diabetes_prevalence,
+           female_smokers,
+           male_smokers,
+           handwashing_facilities,
+           hospital_beds_per_thousand,
+           life_expectancy,
+           human_development_index)
+  
+  populationData <- split(populationData, populationData$Country)
+  populationData <- lapply(populationData, function(d){
+    d <- d %>% distinct()
+    d
+  })
+  populationData <- bind_rows(populationData)
+  return(populationData)
+}
+
+getEvents <- function(allData){
+  eventsData <- allData %>%
+    select(Country,
+           date,
+           total_cases,
+           new_cases,
+           new_cases_smoothed,
+           total_deaths,
+           new_deaths,
+           new_deaths_smoothed,
+           reproduction_rate,
+           icu_patients,
+           hosp_patients,
+           weekly_icu_admissions,
+           weekly_hosp_admissions,
+           total_tests,
+           new_tests,
+           new_tests_smoothed,
+           positive_rate,
+           tests_per_case,
+           tests_units,
+           total_vaccinations,
+           people_vaccinated,
+           people_fully_vaccinated,
+           new_vaccinations,
+           new_vaccinations_smoothed)
+  eventsData <- melt(eventsData, id=seq(1,2),
+                     measure=seq(3,ncol(eventsData)))
+  return(eventsData)
+}
+
+updateDb <- function(){
+  
+  allData <- getAllData()
+  populationData <- getPopulation(allData)
+  eventsData <- getEvents(allData)
+  con <- dbConnect(RSQLite::SQLite(), "testdb")
+  
+  dbWriteTable(con, "population",
+               populationData, overwrite = TRUE)
+  dbWriteTable(con, "events",
+               eventsData, overwrite = TRUE)
+  
+  dbDisconnect(con)
+  
+}
+
+updateDb()
+
+getEventsDb <- function(countries, metrics){
+  con <- dbConnect(RSQLite::SQLite(), "testdb")
+  sqltxt <- sprintf("SELECT * FROM events WHERE Country IN('%s') AND variable IN('%s')",
+                   paste(countries, collapse = "','"),
+                   paste(metrics, collapse = "','"))
+                   
+  res <- dbSendQuery(con, sqltxt)
+  res <- dbFetch(res)
+  res$date <- as.Date(res$date)
+  dbDisconnect(con)
+  return(res)
+}
+
+getPopulationDb <- function(countries){
+  con <- dbConnect(RSQLite::SQLite(), "testdb")
+  sqltxt <- sprintf("SELECT * FROM population WHERE Country IN('%s')",
+                    paste(countries, collapse = "','"))
+  
+  res <- dbSendQuery(con, sqltxt)
+  res <- dbFetch(res)
+  dbDisconnect(con)
+  return(res)
+}
+
+events <- getEventsDb(c('Canada', 'Italy'),
+                      c('total_cases', 'total_vaccinations'))
+population <- getPopulationDb(c('Canada', 'Italy'))
+
+# to do
+#plotting function
